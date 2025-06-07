eb37e9594dc1e2412dc3dbe22ce9a3deb6740d30
HSYlee <HSYlee@example.com>
2025-06-07 16:21:39 +0800

    make a js function

---
+++ selfweb/HSYlee.js
@@ -0,0 +1,9 @@
+const text1 = document.querySelector(".hetext")
+function changeopacity(){
+    if(text1.style.opacity == 0){
+        text1.style.opacity = 1;
+    }
+    else{
+        text1.style.opacity = 0;
+    }
+}
